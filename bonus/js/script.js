ca4747ae4dc101d916368fd11706c0b80fe5e7e1
daniele.gattuso <d.gattuso09@gmail.com>
2025-01-10 17:21:14 +0100

    done bonus

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -5,7 +5,7 @@
 
 const bottone = document.querySelector('.button');
 
-
+let isActive = false
 
 // Gestiamo l'evento
 
@@ -13,10 +13,23 @@ bottone.addEventListener('click', function(){
 // Vogliamo accendewre la lampadina cambaindo immagine con una funzione
 const cambiareImmagine = document.getElementById("immagine1")
 
-
+isActive = !isActive
 // Cambiamo src immagine
-cambiareImmagine.src = "./img/yellow_lamp.png"
 
+
+
+
+if(isActive){
+
+    cambiareImmagine.src = "./img/yellow_lamp.png"
+    bottone.textContent = "Spenta"
+
+}else{
+    cambiareImmagine.src = "./img/white_lamp.png"
+    bottone.textContent = "Accesa"
+
+}
+   
 
 
 })
